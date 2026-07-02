542a15bf14216d84553077ecb8dcadc7994ded79
felipeha323 <60258356+felipeha323@users.noreply.github.com>
2026-07-01 21:49:04 -0500

    Implementing the logic for the algorithm.

--- src/main/java/com/tempo/hierarchy/service/HierarchyFilter.java
+++ src/main/java/com/tempo/hierarchy/service/HierarchyFilter.java
@@ -24,8 +24,41 @@ public class HierarchyFilter {
             }
 
 
+            List<Integer> resultNodeIds = new ArrayList<>();
+            List<Integer> resultDepths = new ArrayList<>();
 
-            return new ArrayBasedHierarchy(new int[0], new int[0]);
+            Deque<Boolean> ancestorValidityStack = new ArrayDeque<>();
+
+            for (int i=0; i < hierarchy.size(); i++) {
+                int nodeId = hierarchy.nodeId(i);
+                int depth = hierarchy.depth(i);
+
+                //Pop the stack back to the level of this node's parent.
+
+                while(ancestorValidityStack.size() > depth) {
+                    ancestorValidityStack.pop();
+                }
+
+                //Check if immediate parent is valid.
+                boolean parentIsValid = ancestorValidityStack.isEmpty() || ancestorValidityStack.peek();
+
+                //This node is valid if the parent is valid +and it passes the predicate.
+                boolean isValid = parentIsValid && nodeIdPredicate.test(nodeId);
+
+                // Stack the result of this node, which will be the parent in the next iteration.
+                ancestorValidityStack.push(isValid);
+
+                if(isValid) {
+                    resultNodeIds.add(nodeId);
+                    resultDepths.add(depth);
+                }
+            }
+
+
+            int[] finalNodeIds = resultNodeIds.stream().mapToInt(Integer::intValue).toArray();
+            int[] finalDepths = resultDepths.stream().mapToInt(Integer::intValue).toArray();
+
+            return new ArrayBasedHierarchy(finalNodeIds, finalDepths);
         }
     }
 
